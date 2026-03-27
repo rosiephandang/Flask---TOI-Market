998bf21203e7e87740b3a80360a59d1af6cec4b4
rosiephandang <23598@burnside.school.nz>
2026-03-27 14:47:19 +1300

    added basic flask and home html page

--- app.py
+++ app.py
@@ -1,1 +1,36 @@
-print("Let's gooo")
+import sqlite3
+from flask import Flask, g, render_template
+
+DATABASE = 'database.db'
+
+#initialise app
+app = Flask(__name__)
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close() 
+    return (rv[0] if rv else None) if one else rv
+
+
+@app.route('/')
+def home():
+    # home page
+    sql = """SELECT * FROM users;"""
+    results = query_db(sql)
+    return render_template("home.html", results=results)
+
+if __name__ == "__main__":
+    app.run(debug=True)
